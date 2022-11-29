7aa386146b740f0dfacf4a972cde41253e8ecc99
AndrewLin95 <andrew.lin88@hotmail.com>
2022-11-29 14:26:56 -0500

    added projects component

---
+++ components/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div>
+      <div className="text-3xl text-blue-400 py-6">Projects</div>
+    </div>
+  );
+};
+
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import IntroductionStatement from '../components/IntroductionStatement';
 import About from '../components/About';
 import Technologies from '../components/Technologies';
+import Projects from '../components/Projects/Projects';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
@@ -17,6 +18,8 @@ const Home: NextPage = () => {
       <About />
 
       <Technologies />
+
+      <Projects />
     </div>
 
     // <div className="flex min-h-screen flex-col items-center justify-center py-2">
