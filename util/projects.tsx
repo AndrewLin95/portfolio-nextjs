ca4223f9c2e468df537cc2aa0ed4edb3d14d7e0a
AndrewLin95 <andrew.lin88@hotmail.com>
2022-11-29 18:19:19 -0500

    added project information for Spotify Clone and Hotpot buddy

--- util/projects.tsx
+++ util/projects.tsx
@@ -1,5 +1,31 @@
 export const PROJECTS = [
   {
+    projectName: 'Spotify Clone',
+    image: 'SpotifyClone.PNG',
+    altDescription: 'Spotify Clone',
+    paragraph1:
+      "Heavily using Spotify's APIs and Web Playback SDK, re-created Spotify mimicking core functionality including the ability to playback music, full search functionality, playlist retrieval and more.",
+    paragraph2:
+      'Optimized handling of states and props in React by creating small re-usable components. This allowed me to scale the application while keeping the code base small.',
+    builtUsing:
+      'Built using Typescript, React, AWS (S3, Lambda, DynamoDB, API Gateway), MUI, Spotify API, Spotify Web SDK',
+    liveLink: 'http://al.spotify-clone.s3-website.us-east-2.amazonaws.com/',
+    githubLink: 'https://github.com/AndrewLin95/spotify_clone'
+  },
+  {
+    projectName: 'Hotpot Buddy',
+    image: 'HotpotBuddy.PNG',
+    altDescription: 'Hot Pot Buddy',
+    paragraph1:
+      'Collaborated, within a small team, a mobile-focused web application that assists users in determining when a food item is cooked.',
+    paragraph2:
+      'Designed and planned using Jira with a robust versioning system and roadmaps. An extensive list of upcoming features and identified issues based on feedback collected from test users. Heavily version controlled using Git',
+    builtUsing:
+      'Built using Typescript, React, NodeJs, AWS (S3, Lambda, DynamoDB, API Gateway), MUI',
+    liveLink: 'https://reddragonsquad.github.io/Hotpot-Buddy/',
+    githubLink: 'https://github.com/RedDragonSquad/Hotpot-Buddy'
+  },
+  {
     projectName: 'Find the Object!',
     image: 'WheresWaldo.PNG',
     altDescription: "Where's Waldo Clone",
