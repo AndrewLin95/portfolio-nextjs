ab1bc14b07dac69e2b86f844eb5ffef2e8d52481
AndrewLin95 <andrew.lin88@hotmail.com>
2022-11-29 17:56:55 -0500

    refactored to be able to receive a object as input to generate projects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,10 +1,19 @@
 import ProjectsContainer from './ProjectsContainer';
 
+import { PROJECTS } from '../../util/projects';
+
 const Projects = () => {
   return (
     <div>
       <div className="text-3xl text-blue-400 py-6">Projects</div>
-      <ProjectsContainer />
+      {Object.entries(PROJECTS).map(([key, data]) => {
+        console.log(data);
+        return (
+          <div key={key}>
+            <ProjectsContainer data={data} />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- components/Projects/ProjectsContainer.tsx
+++ components/Projects/ProjectsContainer.tsx
@@ -1,49 +1,42 @@
-const ProjectsContainer = () => {
+import { projectsInterface } from '../../util/models';
+import { FC } from 'react';
+
+interface Props {
+  data: projectsInterface;
+}
+
+const ProjectsContainer: FC<Props> = ({ data }) => {
   return (
     <div className="flex flex-row">
       <img
         className="h-80 opacity-60"
-        src={`static/assets/WheresWaldo.PNG`}
-        alt={`Where's Waldo Clone`}
+        src={`static/assets/${data.image}`}
+        alt={data.altDescription}
         loading="eager"
       />
       <div className="flex flex-col w-screen relative right-52 top-4">
         <div className="h-8 relative left-8 bg-indigo-900 w-48 px-3 py-1">
           <div className="flex justify-start items-center">
-            Find the Object!
+            {data.projectName}
           </div>
         </div>
         {/* Description */}
         <div className="bg-custom-2 p-5 flex flex-col gap-3 text-sm">
-          <p>
-            A fully responsive full stack photo tagging web application game.
-          </p>
-          <p>
-            The data points are stored using absolute values in Googles's Cloud
-            Firestore NoSQL database where the values are transformed based on
-            the relative position of the browser viewport.
-          </p>
+          <p>{data.paragraph1}</p>
+          <p>{data.paragraph2}</p>
 
           {/* Divider */}
           <div className="border border-gray-400 w-full min-w-80"></div>
 
           {/* Built Using */}
-          <p className="italic">
-            Built using Typescript, React and Google Firebase
-          </p>
+          <p className="italic">{data.builtUsing}</p>
 
           {/* Links */}
           <div className="flex flex-row gap-4 justify-end text-base">
-            <a
-              href="https://andrewlin95.github.io/wheres_waldo/"
-              target={`_blank`}
-            >
+            <a href={data.liveLink} target={`_blank`}>
               <div>Live Site</div>
             </a>
-            <a
-              href="https://github.com/AndrewLin95/wheres_waldo/"
-              target={`_blank`}
-            >
+            <a href={data.githubLink} target={`_blank`}>
               <div>Github</div>
             </a>
           </div>
