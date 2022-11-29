6093fb44f03cdf4ece8dede7461586b3051ed823
AndrewLin95 <andrew.lin88@hotmail.com>
2022-11-29 17:57:14 -0500

    created a projects object to be used to render

---
+++ util/models.tsx
@@ -0,0 +1,10 @@
+export interface projectsInterface {
+  projectName: string;
+  image: string;
+  altDescription: string;
+  paragraph1: string;
+  paragraph2: string;
+  builtUsing: string;
+  liveLink: string;
+  githubLink: string;
+}
---
+++ util/projects.tsx
@@ -0,0 +1,14 @@
+export const PROJECTS = [
+  {
+    projectName: 'Find the Object!',
+    image: 'WheresWaldo.PNG',
+    altDescription: "Where's Waldo Clone",
+    paragraph1:
+      'A fully responsive full stack photo tagging web application game.',
+    paragraph2:
+      "The data points are stored using absolute values in Googles's Cloud Firestore NoSQL database where the values are transformed based on the relative position of the browser viewport.",
+    builtUsing: 'Built using Typescript, React and Google Firebase',
+    liveLink: 'https://andrewlin95.github.io/wheres_waldo/',
+    githubLink: 'https://github.com/AndrewLin95/wheres_waldo/'
+  }
+];
