5b2e90d14fdf6ceba94c0d194d5dff2b7540f80b
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-04 13:20:48 -0500

    added parallax to project descriptions

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -5,8 +5,8 @@ import { PROJECTS } from '../../util/projects';
 const Projects = () => {
   return (
     <div className="pb-36">
-      <div className="text-5xl text-blue-400 py-6">Projects</div>
-      <div className="flex flex-col gap-36 w-full relative lg:left-28 sm:left-8">
+      <div className="text-5xl text-blue-400 py-6 pb-32">Projects</div>
+      <div className="flex flex-col gap-[450px] w-full relative lg:left-28 sm:left-8">
         {Object.entries(PROJECTS).map(([key, data]) => {
           return (
             <div key={key}>
--- components/Projects/ProjectsContainer.tsx
+++ components/Projects/ProjectsContainer.tsx
@@ -1,5 +1,6 @@
 import { projectsInterface } from '../../util/models';
 import { FC } from 'react';
+import { ScrollParallax } from 'react-just-parallax';
 
 interface Props {
   data: projectsInterface;
@@ -14,34 +15,36 @@ const ProjectsContainer: FC<Props> = ({ data }) => {
         alt={data.altDescription}
         loading="eager"
       />
-      <div className="flex flex-col w-screen relative right-52">
-        <div className="h-8 relative left-8 bg-indigo-900 w-48 px-3 py-1">
-          <div className="flex justify-start items-center">
-            {data.projectName}
+      <ScrollParallax>
+        <div className="flex flex-col w-full relative right-52">
+          <div className="h-8 relative left-8 bg-indigo-900 w-48 px-3 py-1">
+            <div className="flex justify-start items-center">
+              {data.projectName}
+            </div>
+          </div>
+          {/* Description */}
+          <div className="bg-custom-2 p-5 flex flex-col gap-3 text-sm">
+            <p>{data.paragraph1}</p>
+            <p>{data.paragraph2}</p>
+
+            {/* Divider */}
+            <div className="border border-gray-400 w-full min-w-80"></div>
+
+            {/* Built Using */}
+            <p className="italic">{data.builtUsing}</p>
+
+            {/* Links */}
+            <div className="flex flex-row gap-4 justify-end text-base">
+              <a href={data.liveLink} target={`_blank`}>
+                <div>Live Site</div>
+              </a>
+              <a href={data.githubLink} target={`_blank`}>
+                <div>Github</div>
+              </a>
+            </div>
           </div>
         </div>
-        {/* Description */}
-        <div className="bg-custom-2 p-5 flex flex-col gap-3 text-sm">
-          <p>{data.paragraph1}</p>
-          <p>{data.paragraph2}</p>
-
-          {/* Divider */}
-          <div className="border border-gray-400 w-full min-w-80"></div>
-
-          {/* Built Using */}
-          <p className="italic">{data.builtUsing}</p>
-
-          {/* Links */}
-          <div className="flex flex-row gap-4 justify-end text-base">
-            <a href={data.liveLink} target={`_blank`}>
-              <div>Live Site</div>
-            </a>
-            <a href={data.githubLink} target={`_blank`}>
-              <div>Github</div>
-            </a>
-          </div>
-        </div>
-      </div>
+      </ScrollParallax>
     </div>
   );
 };
